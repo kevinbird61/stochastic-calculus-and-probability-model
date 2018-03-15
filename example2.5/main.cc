3748fb90d08f8ddca4c0eda87b996a3e15260040
kevinbird61 <kevinbird61@gmail.com>
2018-03-15 21:34:40 +0800

    Add first commit.
    * With example 2.5 prototype.

---
+++ example2.5/main.cc
@@ -0,0 +1,76 @@
+// standard lib
+#include <cstdio>
+#include <unistd.h>
+#include <time.h>
+#include <cstdlib>
+#include <cstring>
+// self-defined
+#include <ansi_color.h>
+#define DEFAULT_ROUNDS 100
+#define DEFAULT_RANGES 10
+
+void helper(FILE *fp,char *prog);
+int checker();
+
+int main(int argc,char *argv[]){
+    // specify the n rounds, in range: 1~m
+    int c,n=DEFAULT_ROUNDS,m=DEFAULT_RANGES;
+    // parsing arguments
+    while((c=getopt(argc,argv,"n:m:h"))!=-1){
+        switch(c){
+            case 'n':
+                if(optarg) 
+                    n=std::atoi(optarg);
+                break;
+            case 'm':
+                if(optarg) 
+                    m=std::atoi(optarg);
+                break;
+            case 'h':
+                helper(stdout,argv[0]);
+                exit(1);
+                break;
+            default:
+                helper(stderr,argv[0]);
+                exit(EXIT_FAILURE);
+        }
+    }
+    // print 
+    printf("Rounds: %d, Range: 1~%d\n",n,m);
+
+    int count=0,*count_arr,total=0;
+    count_arr=(int *)calloc(m,sizeof(int));
+    // run testcase time
+    while(n--){        
+        memset(count_arr,0,m*sizeof(int));
+        // need a totally random generator
+        srand(time(NULL)+rand());
+        while(count<m){
+            int rnd=rand()%m;
+            //printf("Count: %d, Random %d\n",count,rnd);
+            if(count_arr[rnd]==0) count++;
+            count_arr[rnd]++;
+            // total sum
+            total++;
+        }
+        // get summation 
+        for(int i=0;i<m;i++){
+            printf("%d: %d(%.3f)\n",i,count_arr[i],(float)count_arr[i]/total);
+        }
+        printf("Total steps: %d\n",total);
+        count=0;
+        total=0;
+    }
+
+    return 0;
+}
+
+void helper(FILE *fp,char *prog){
+    fprintf(fp,
+        ANSI_COLOR_RED"Usage: %s [-n rounds] [-m range] [-h]"ANSI_COLOR_RESET"\n\n%s\n%s\n%s\n",
+        prog,
+        ANSI_COLOR_GREEN"  -n rounds"ANSI_COLOR_RESET":\tspecify how many times you want to run the simulation routine.",
+        ANSI_COLOR_GREEN"  -m range"ANSI_COLOR_RESET":\tspecify the range of each execution.",
+        ANSI_COLOR_GREEN"  -h "ANSI_COLOR_RESET":\tprint out the helper information."
+        );
+}
